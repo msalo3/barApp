d7e880b49855147a0886d5a3bc28489359c561a9
Marc Salo <marc@nurelm.com>
2017-11-24 14:11:42 -0500

    Adds linter

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { createStore } from 'redux';
+import { AsyncStorage } from 'react-native';
+import devTools from 'remote-redux-devtools';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { persistStore, autoRehydrate, purgeStoredState } from 'redux-persist';
 import { StyleSheet, Text, View } from 'react-native';
 import reducers from './reducers';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      store: this.setupStore(() => this.setState({ isLoading: false }))
+    };
+  }
+
+  setupStore(onCompletion:()=>void):any {
+    const enhancer = compose(
+      applyMiddleware(thunk),
+      autoRehydrate(), // Un/comment for persistent state via LocalStorage
+      devTools({
+        name: 'BarApp', realtime: true
+      }),
+    );
+
+    const store = createStore(reducer, enhancer);
+    persistStore(store, { storage: AsyncStorage }, onCompletion);
+
+    return store;
+  }
+
   render() {
     return (
       <View style={styles.container}>
